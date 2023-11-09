832c196b126e8f302ca867327af4cc91e9b0f4a1
Extradev <scott.birner@gmail.com>
2023-11-09 12:47:46 +0100

    Add NgOptimizedImage

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,17 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Todo } from '../shared/todo.model';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgOptimizedImage],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss'
 })
 export class TodoComponent {
-  @Input() todo: Todo | undefined;
+  @Input() todo!: Todo;
 
-  constructor() {
+  constructor() { }
+
+  toggleTodo() {
+    this.todo.status = !this.todo.status
   }
 }
